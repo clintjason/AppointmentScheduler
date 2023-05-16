3342ff9812df2f408741ee1710d55ed2ff9aff66
CJ <ngassajason@yahoo.fr>
2023-05-16 11:55:17 +0100

    💄 adds notification system on record creation

--- src/components/CreateRecord/index.jsx
+++ src/components/CreateRecord/index.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import dayjs from 'dayjs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import Header from '../Header';
-import { Typography, Divider, TimePicker, DatePicker, Button, Form, Input, Select } from 'antd';
-import React from 'react';
+import { Typography, Divider, TimePicker, DatePicker, Button, Form, Input, Select, notification } from 'antd';
+import { useState, useEffect } from 'react';
+
+import PhoneInput from "antd-phone-input";
+
+const phoneValidationRegex = /\d{3}-\d{3}-\d{4}/;
 
 const { Option } = Select;
 const layout = {
@@ -29,7 +33,22 @@ const CreateRecord = ({pageTitle}) => {
   document.title = pageTitle;
   const { Title } = Typography;
 
+  const [api, contextHolder] = notification.useNotification();
+
+  const openNotificationWithIcon = (type) => {
+    const suc = "The Appointment was successfully created. Visit the Dashboard to view and edit it if need be.";
+    const fail = "The appointment was not created successfully. Please try again later.";
+    const desc = type.toLowerCase() == 'error' ? fail : suc;
+    api[type]({
+      message: `Appointment Registration  ${type}`,
+      description: desc,
+    });
+  }
+
+  useEffect(() => {})
+
   const [form] = Form.useForm();
+  const [id, setId] = useState(0);
   const onGenderChange = (value) => {
     switch (value) {
       case 'male':
@@ -52,8 +71,32 @@ const CreateRecord = ({pageTitle}) => {
 
   const onFirstTimeChange = (value) => {}
   const onAppointmentStatusChange = (value) => {}
+
   const onFinish = (values) => {
     console.log(values);
+    const list = values.appointment_date;
+    console.log(list);
+    console.log(list.$D);
+    console.log(list.$M);
+    console.log(list.$y);
+    //  create a counter to count the numbers of times the form is submitted successfully
+    // assign it that id to the field uniquecode. create a function that will compute the new vlaue each time
+    setId(id + 1);
+    const unique_code = "A" + id + list.$D + list.$M + list.$y;
+    const data = {
+      ...values,
+      unique_code: unique_code,
+    }
+    // send to backend server
+    axios.post('http://localhost:3000/api/appointment/new', data)
+    .then(response => {
+      console.log(response);
+      openNotificationWithIcon('success');
+    })
+    .catch(error => {
+      console.log(error);
+      openNotificationWithIcon('error');
+    })
   };
   const onReset = () => {
     form.current?.resetFields();
@@ -65,33 +108,38 @@ const CreateRecord = ({pageTitle}) => {
     });
   };
 
+  const onRequestDateChange = (value) => {
+    form.current?.setFieldsValue({ request_date:value})
+  }
+
   return (
     <>
       <Header borderRadius="12px 0px" maxWidth="80%" width="auto" />
+      {contextHolder}
       <main>
         <div className='flex-wrapper center-all'>
           <Link className="gtl" to="/"><FontAwesomeIcon icon={faArrowLeft} /></Link>
           <Title level={3} className='appointment__title'>NEW RECORD</Title>
         </div>
         <Form
-          ref={form}
+          form={form}
           name="control-ref"
           onFinish={onFinish}
           className="flex-wrapper"
-          
         >
           <Title level={4} className='form__heading'>General Information</Title>
           <Form.Item
-            name="UniqueCode"
+            name="unique_code"
             label="Unique Code"
             className="column-wrapper"
           >
-            <Input disabled={true} />
+            <Input disabled={true}  />
           </Form.Item>
           <Form.Item
             name="name"
             label="Name"
             className="column-wrapper"
+            tooltip="This is a required field"
             rules={[
               {
                 required: true,
@@ -129,7 +177,7 @@ const CreateRecord = ({pageTitle}) => {
               },
             ]}
           >
-            <Input placeholder="Enter Phone N°"/>
+            <Input placeholder="Enter Phone N°" type="phone"/>
           </Form.Item>
           <Form.Item
             name="email"
@@ -148,12 +196,13 @@ const CreateRecord = ({pageTitle}) => {
           <Title level={4} className='form__heading'>Appointment Information</Title>
           <Form.Item 
             label="Appointment date"
+            name="appointment_date"
             className="column-wrapper"
           >
             <DatePicker />
           </Form.Item>
           <Form.Item
-            name="first-time"
+            name="first_time"
             label="First time"
             className="column-wrapper"
             rules={[
@@ -173,12 +222,13 @@ const CreateRecord = ({pageTitle}) => {
           </Form.Item>
           <Form.Item 
             label="Request date"
+            name="request_date"
             className="column-wrapper"
           >
-            <DatePicker />
+            <DatePicker onChange={onRequestDateChange}/>
           </Form.Item>
           <Form.Item
-            name="appointment-status"
+            name="appointment_status"
             label="Appointment Status"
             className="column-wrapper"
             rules={[
@@ -200,8 +250,9 @@ const CreateRecord = ({pageTitle}) => {
           <Form.Item 
             label="Appointment time"
             className="column-wrapper"
+            name="appointment_time"
           >
-            <TimePicker defaultValue={dayjs('12:08', format)} format={format} />
+            <TimePicker format={format} />
           </Form.Item>
           <Title level={4} className='form__heading'>Address Information</Title>
           <Form.Item
@@ -229,23 +280,38 @@ const CreateRecord = ({pageTitle}) => {
             <Input placeholder="Enter City"/>
           </Form.Item>
           <Title level={4} className='form__heading'>Notes</Title>
-          <div className="column-wrapper">
-            <label htmlFor="">Before appointment</label>
-            <textarea className="txt-area" rows={4} name="before-appointment" placeholder="Notes Before appointment"></textarea>
-          </div>
-          <div className="column-wrapper">
+          <Form.Item
+            label="Before appointment"
+            name="before_appointment"
+          >
+            <Input.TextArea
+              placeholder="Message ..."
+              autoSize={{ minRows: 4, maxRows: 6 }}
+            />
+          </Form.Item>
+          <Form.Item
+            label="After appointment"
+            name="after_appointment"
+            className="column-wrapper"
+          >
+            <Input.TextArea
+              placeholder="Message ..."
+              autoSize={{ minRows: 4, maxRows: 6 }}
+            />
+          </Form.Item>
+          {/* <div className="column-wrapper">
             <label htmlFor="">After appointment</label>
             <textarea className="txt-area" rows={4} name="after-appointment" placeholder="Notes After appointment"></textarea>
-          </div>
+          </div> */}
           <Form.Item className="submit-wrapper">
             <Button className="submit-color" htmlType="submit">
               Save
             </Button>
           </Form.Item>
-    </Form>
+        </Form>
       </main>
     </>
   )
 }
 
-export default CreateRecord
+export default CreateRecord;
